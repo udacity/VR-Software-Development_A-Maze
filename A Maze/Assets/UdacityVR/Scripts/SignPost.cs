0575585406efac12d8e40e2a41942e403f0663d2
Kristin Dragos <KristinDragos@gmail.com>
2019-01-17 15:04:07 -0700

    chore: Clean up unneeded comment from sign post script.
    Remove extra comment from SignPost.cs that didn't belong there.

--- A Maze/Assets/UdacityVR/Scripts/SignPost.cs
+++ A Maze/Assets/UdacityVR/Scripts/SignPost.cs
@@ -12,7 +12,6 @@ public class SignPost : MonoBehaviour {
 
 		// Prints to the console when the method is called
 		Debug.Log ("'SignPost.ResetScene()' was called");
-		// Use 'leftDoor' to get the start rotation of the 'Left_Door' game object and assign it to 'leftDoorStartRotation'
 
 		// TODO: Reset the scene by getting a reference to the scene and reloading it
 		// Declare a Scene named 'scene', then use SceneManager.GetActiveScene () to get the current scene and assign it to 'scene'
